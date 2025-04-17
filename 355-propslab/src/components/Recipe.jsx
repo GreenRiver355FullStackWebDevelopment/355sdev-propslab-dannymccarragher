11bd24e59f83c1eeacb7e69e0e84a000bc9b6fe6
dannymccarragher <dannymccarragher@gmail.com>
2025-04-17 11:18:32 -0700

    Added Styling

--- 355-propslab/src/components/Recipe.jsx
+++ 355-propslab/src/components/Recipe.jsx
@@ -1,11 +1,12 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { CardHeader } from "@mui/material";
+import Typography from "@mui/material";
 
 const Recipe = ({ name, ingredients, instructions }) => {
   return (
-    <Card sx={{ margin: 10 }}>
-      <CardHeader title={name}></CardHeader>
+    <Card sx={{ margin: 10 }} variant="outlined">
+      <CardHeader title={name} color='red'></CardHeader>
       <CardContent>
         {ingredients}
       </CardContent>
--- 355-propslab/src/components/RecipeContainer.jsx
+++ 355-propslab/src/components/RecipeContainer.jsx
@@ -1,5 +1,7 @@
 import Recipe from "./Recipe.jsx";
 
+
+
 const RecipeContainer = ({recipes}) => {
     return(
         <>
